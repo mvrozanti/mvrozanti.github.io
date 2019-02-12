b1f4da6373008ca41da95d4d6d24ba9b48bc745b
Marcelo <mvrozanti@hotmail.com>
2019-02-12 10:53:48 -0200

    updated rem-foot

--- rem-foot.sh
+++ rem-foot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find public -type f -exec perl -i -p0e 's/<p class="powered-by">.*?<\/p>//s' {} \;
+find public -type f -exec perl -i -p0e 's/<p class="powered-by">.*?<\/p>//s;s/<copyright>.*?<\/copyright>//s;s/Minha landing page \&middot\;//s;s/<a href="https:\/\/gohugo.io" target="_blank" rel="noopener">Hugo<\/a>\.//s' {} \;
