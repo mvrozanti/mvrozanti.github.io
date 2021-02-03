d17c8c058d3de147dbff0aea3f3e4f37c17141e5
Marcelo <mvrozanti@hotmail.com>
2021-02-02 22:00:11 -0300

    post-wowchemy update

--- update_academic.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Display available updates to Academic.
-cd themes/academic
-git fetch
-git log --pretty=oneline --abbrev-commit --decorate HEAD..origin/master
-
-# Update Academic.
-git submodule update --remote --merge
