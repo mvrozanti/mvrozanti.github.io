670ddc037ff44d85e86514e9ebfa5ce22e84f7e1
Marcelo <mvrozanti@hotmail.com>
2025-09-06 19:01:18 -0300

    ???

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
-    env: {
-        TOKEN_GITHUB: process.env.TOKEN_GITHUB,
-    }
+  },
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has TypeScript errors.
+    ignoreBuildErrors: true,
+  },
+  env: {
+    TOKEN_GITHUB: process.env.TOKEN_GITHUB,
+  }
 };
 
 export default nextConfig;
