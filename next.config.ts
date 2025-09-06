c6894b4aafc2cc33e78ce54fb63a6ea2f2ea5806
Marcelo <mvrozanti@hotmail.com>
2025-09-06 17:14:02 -0300

    ???

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    reactStrictMode: true,
     ignoreDuringBuilds: true,
     env: {
         TOKEN_GITHUB: process.env.TOKEN_GITHUB,
